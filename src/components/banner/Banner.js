10c06e767262926a8e0d1ed1a2980e28c7369bb5
jsy3437 <joseoyun12@gmail.com>
2021-09-08 23:16:19 +0900

    banner : fade, navbar : mobile

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 import banner1 from '../../images/banner1.jpg';
 import banner2 from '../../images/banner2.jpg';
 import banner3 from '../../images/banner3.jpg';
@@ -7,76 +8,97 @@ import banner5 from '../../images/banner5.jpg';
 import banner6 from '../../images/banner6.jpg';
 
 function Banner() {
-	let [number, setNumber] = useState(0);
+	let [number, setNumber] = useState([0, 0]);
 	let banner = [banner1, banner2, banner3, banner4, banner5, banner6];
 
-	// let [imgArray, setImgArray] = useState([]);
-	let [imgArray, setImgArray] = useState(1);
-
+	let toggleImg = document.querySelector('.imgSlider_img');
 	//
-	let i;
 	let time;
+
 	// 1. 3초마다 setImgArray로 0번째 빼서 제일뒤로 옮기기
 	// 2. 0번째 부터 5번째 까지 3초마다 반복문 돌려서 5번째가 되면 다시 0으로 만들어 주기
 
+	// 4초마다 이미지 띄워주는 함수 실행
 	useEffect(() => {
-		time = setTimeout(() => {
-			console.log('aa');
-			bannerFade();
-		}, 4000);
+		if (number[1] !== 1) {
+			time = setTimeout(() => {
+				bannerFade();
+				{
+					toggleImg && toggleImg.classList.remove('fade');
+				}
+			}, 4000);
+		} else if (number[1] == 1) {
+			resetNumber();
+		}
+		return () => {
+			clearTimeout(time);
+
+			{
+				toggleImg && toggleImg.classList.add('fade');
+			}
+		};
 	}, [number]);
 
+	function resetNumber() {
+		setNumber([number[0], 0]);
+	}
+
+	// 이미지 바꿔주는 함수
 	function bannerFade() {
-		if (number == 5) {
-			setNumber(0);
+		if (number[0] == 5) {
+			setNumber([0, number[1]]);
 		} else {
-			setNumber(number + 1);
+			setNumber([number[0] + 1, number[1]]);
 		}
 	}
 
-	// 버튼누르면 중첩되니까 그거 없어지게 하기
+	// 배너 next버튼 누르면 실행
+	function clickNextBnt() {
+		if (number[0] == 5) {
+			setNumber([0, 1]);
+		} else {
+			setNumber([number[0] + 1, 1]);
+		}
+		{
+			toggleImg && toggleImg.classList.add('fade');
+		}
+		{
+			toggleImg && toggleImg.classList.remove('fade');
+		}
+	}
 
-	// for (var i = 0; i < 6; i++) {
-	// 	let time = setTimeout(() => {
+	// 배너 Prev 버튼 누르면 실행
+	function clickPrevBnt() {
+		if (number[0] == 0) {
+			setNumber([5, 1]);
+		} else {
+			setNumber([number[0] - 1, 1]);
+		}
+		{
+			toggleImg && toggleImg.classList.add('fade');
+		}
+		{
+			toggleImg && toggleImg.classList.remove('fade');
+		}
+	}
 
-	// 	});
-	// 	arr.push(
-	// 		<div>
-	// 			<img src={imgArray[i]} />
-	// 		</div>
-	// 	);
-	// }
-
-	// useEffect(() => {
-	// 	let time = setTimeout(() => {
-	// 		//
-	// 	}, 3000);
-	// 	return () => {
-	// 		clearTimeout(time);
-	// 	};
-	// }, []);
-
-	// function showImg() {}
-	console.log(banner1, 'banner1');
 	return (
 		<div className='banner'>
 			<div className='imgSlider'>
-				<ul>
-					<div>
-						<img src={banner[number]} />
-					</div>
-				</ul>
+				<div className='imgSlider_img fade'>
+					<img src={banner[number[0]]} />
+				</div>
 			</div>
 
 			<div
 				className='prev-button'
 				onClick={() => {
-					setNumber(number - 1);
+					clickPrevBnt();
 				}}></div>
 			<div
 				className='next-button'
 				onClick={() => {
-					setNumber(number + 1);
+					clickNextBnt();
 				}}></div>
 		</div>
 	);
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -18,72 +18,83 @@ function Navbar() {
 
 	return (
 		<div>
-			<div className='navbar'>
-				<div className='navbar_menu'>
-					<div className='navbar_menu_delivery'>
-						서울,경기,인천 지역&nbsp;<div className='point'>새벽배송☞</div>
-					</div>
-					<div className='menu'>
-						<li>
-							<a>로그인</a>
-						</li>
-						<li>
-							<a>회원가입</a>
-						</li>
-						<li className='service' >
-							<a>
-								고객센터
-							</a><img src='https://okkot.com/images/common/top_arrow.png'></img>
-							<div className='service_drop'>
-								<ul>
-									<li>
-										<a>공지사항</a>
-									</li>
-									<li>
-										<a>이벤트</a>
-									</li>
-									<li>
-										<a>FAQ</a>
-									</li>
-								</ul>
-							</div>
-						</li>
-						{/* {navbarMenus.map(function (navbarMenu, i) {
+			<div className='main_navbar'>
+				<div className='navbar'>
+					<div className='navbar_menu'>
+						<div className='navbar_menu_delivery'>
+							서울,경기,인천 지역&nbsp;<div className='point'>새벽배송☞</div>
+						</div>
+						<div className='menu'>
+							<li>
+								<a>로그인</a>
+							</li>
+							<li>
+								<a>회원가입</a>
+							</li>
+							<li className='service'>
+								<a>고객센터</a>
+								<img src='https://okkot.com/images/common/top_arrow.png'></img>
+								<div className='service_drop'>
+									<ul>
+										<li>
+											<a>공지사항</a>
+										</li>
+										<li>
+											<a>이벤트</a>
+										</li>
+										<li>
+											<a>FAQ</a>
+										</li>
+									</ul>
+								</div>
+							</li>
+							{/* {navbarMenus.map(function (navbarMenu, i) {
 							return (
 								<div className='login' key={i}>
 									{navbarMenu}
 								</div>
 							);
 						})} */}
+						</div>
+					</div>
+				</div>
+				<div className='main_logo'>
+					<img src='https://okkot.com/images/common/logo.v3.svg' alt='logo' />
+				</div>
+				<div className='category'>
+					<div className='categoryBar'>
+						<div className='hamburger'>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+
+						<div className='categories'>
+							{categories.map(function (category, i) {
+								return (
+									<ul>
+										<li key={i}>{category}</li>
+									</ul>
+								);
+							})}
+						</div>
+
+						<div className='search_bar'>
+							<input className='searchBar' type='text' placeholder='상품과 브랜드를 검색해 주세요!'></input>
+							<div className='searchBtn'>
+								<img src={SearchImg} />
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className='main_logo'>
-				<img src='https://okkot.com/images/common/logo.v3.svg' alt='logo' />
-			</div>
-			<div className='category'>
-				<div className='categoryBar'>
-					<div className='hamburger'>
-						<span></span>
-						<span></span>
-						<span></span>
+			<div className='mobile_navbar'>
+				<div className='mobile_logo'>
+					<div className='mobile_logo_img'>
+						<img src='https://okkot.com/images/common/logo.v3.svg' />
 					</div>
-
-					<div className='categories'>
-						{categories.map(function (category, i) {
-							return (
-								<ul>
-									<li key={i}>{category}</li>
-								</ul>
-							);
-						})}
-					</div>
-
-					<div className='search_bar'>
-						<input className='searchBar' type='text' placeholder='상품과 브랜드를 검색해 주세요!'></input>
-						<div className='searchBtn'>
-							<img src={SearchImg} />
-						</div>
+					<div className='mobile_logo_hamburger'>
+						<div className='hamburger_icon'></div>
 					</div>
 				</div>
 			</div>
